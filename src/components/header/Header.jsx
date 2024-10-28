0fbd78719ced58084af053af2160c9cb3c54cf99
Kevin <kevinJ1910>
2024-10-28 17:16:39 -0500

    Se agrega modelo 3d y se ajusta el header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,9 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuthStore from "../../stores/use-auth-store";
 import logo from "../../assets/images/logo.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'; // Icono de "cerrar sesión"
+
 
 const Header = () => {
     const { user, logout } = useAuthStore();
@@ -24,12 +27,15 @@ const Header = () => {
           <img src={logo} alt="Logo" />
           <h1>Problemas Medioambientales Relacionados con la Tierra</h1>
           <h6>Bienvenido a TerraQuest, {user.displayName}</h6>
-          {user ? (
-            <button className="button-logout" onClick={logout}>Cerrar Sesión</button>
-          ) : (Logeado)}
+          {user && (
+            <button className="button-logout" onClick={logout}>
+              <FontAwesomeIcon icon={faSignOutAlt}/>
+            </button>
+          )}
         </div>
       </div>
     );
+    
   };
   
   export default Header;
--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -1,11 +1,20 @@
 import Header from "../../components/header/header";
 import "./Inicio.css";
+import { Canvas } from "@react-three/fiber";
+import Forest from "./models-3d/Forest";
+import { OrbitControls } from "@react-three/drei";
 
 const Inicio = () => {
   return (
     <>
     <Header />
     <div className="inicio-container">
+      <Canvas>
+        <OrbitControls />
+        <ambientLight />
+        <directionalLight position={[10, 10, 10]} intensity={5} />
+        <Forest />
+      </Canvas>
     </div>
     </>
     
