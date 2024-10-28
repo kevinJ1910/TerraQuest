4e02a35cb6a2688b347ab6168a6c07532ae24dee
Kevin <kevinJ1910>
2024-10-28 11:00:05 -0500

    se reposisiono el header

--- src/pages/inicio/Inicio.jsx
+++ src/pages/inicio/Inicio.jsx
@@ -1,38 +1,16 @@
-// pages/Inicio.jsx
+import Header from "../../components/header/header";
 import "./Inicio.css";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import useAuthStore from "../../stores/use-auth-store";
-import logo from "../../assets/images/logo.png";
-import { div } from "three/webgpu";
 
 const Inicio = () => {
-  const { user, logout } = useAuthStore();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!user) {
-      navigate("/"); // Redirige al login si no hay un usuario autenticado
-    }
-  }, [user, navigate]);
-
-  if (!user) {
-    return <p>Cargando...</p>;
-  }
-
   return (
+    <>
+    <Header />
     <div className="inicio-container">
-      <div className="container-title">
-        <img src={logo} alt="Logo" />
-        <h1>Problemas Medioambientales Relacionados con la Tierra</h1>
-        <h6>Bienvenido a TerraQuest, {user.displayName}</h6>
-        {user ? (
-          <button className="button-logout" onClick={logout}>Cerrar Sesión</button>
-        ) : (Logeado)}
-      </div>
     </div>
+    </>
+    
   );
 };
 
-export default Inicio;
 
+export default Inicio;
